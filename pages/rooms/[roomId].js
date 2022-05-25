02af88158416a7a696b132a7508448e229b529b5
rheehoonkyung <yebrwe@naver.com>
2022-05-25 23:41:28 +0900

    테스트

--- pages/rooms/[roomId].js
+++ pages/rooms/[roomId].js
@@ -1,18 +1,13 @@
 import { useRouter } from 'next/router'
 import { write, read } from '/components/FirebaseDatabase'
-const Room = ({ roomId }) => {
+const Room = () => {
   const router = useRouter()
-  const { roomId } = router.query
-  read('chats', (data) => {
-    console.log(data);
-  })
+  const { roomId } = router.query;
+  
+  // read('chats', (data) => {
+  //   console.log(data);
+  // })
   return <p>Room: {roomId}</p>
-}
-
-export async function getStaticProps({ params: { roomId } }) {
-  return {
-    props: { roomId },
-  };
 }
 
 export default Room
