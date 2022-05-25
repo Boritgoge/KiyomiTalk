ec81da14122f258a556e3f8ee34031fcb4d73358
rheehoonkyung <yebrwe@naver.com>
2022-05-25 23:20:11 +0900

    [변경] 정적매개변수 추가

--- pages/rooms/[roomId].js
+++ pages/rooms/[roomId].js
@@ -1,12 +1,18 @@
 import { useRouter } from 'next/router'
 import { write, read } from '/components/FirebaseDatabase'
-const Room = () => {
+const Room = ({ roomId }) => {
   const router = useRouter()
   const { roomId } = router.query
   read('chats', (data) => {
     console.log(data);
   })
   return <p>Room: {roomId}</p>
+}
+
+export async function getStaticProps({ params: { roomId } }) {
+  return {
+    props: { roomId },
+  };
 }
 
 export default Room
