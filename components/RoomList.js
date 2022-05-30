ab77382c4d17c332fa06e9c5641b60f754913c13
rheehoonkyung <9398216@ict-companion.com>
2022-05-30 17:42:58 +0900

    [추가] 방만들때 인풋 벗어나면 숨김처리

--- components/RoomList.js
+++ components/RoomList.js
@@ -56,6 +56,9 @@ const RoomList = () => {
                 }
               }
             }
+            onBlur={()=> {
+              setInputToggle(false)
+            }}
             ref={inputRef}
           />
         }
