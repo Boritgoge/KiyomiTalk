3a84649b5343aae2491549dcf7b937061164a145
rheehoonkyung <9398216@ict-companion.com>
2022-06-03 10:24:19 +0900

    [삭제] cors 제거

--- next.config.js
+++ next.config.js
@@ -3,19 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['avatars.githubusercontent.com', 'firebasestorage.googleapis.com'],
-  },
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
-          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-        ]
-      }
-    ]
   }
 }
 
