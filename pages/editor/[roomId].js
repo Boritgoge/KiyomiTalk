01ace901653c504c6c94506bc7a6b4d14cd62423
rheehoonkyung <9398216@ict-companion.com>
2022-06-10 11:23:05 +0900

    [추가] 에디터 언어선택 기능 추가

--- pages/editor/[roomId].js
+++ pages/editor/[roomId].js
@@ -2,6 +2,10 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router'
 import dynamic from 'next/dynamic'
 import db, { read, updateByPath } from '/components/common/FirebaseDatabase'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faJs, faJava, faPython } from "@fortawesome/free-brands-svg-icons"
+import { faDatabase } from "@fortawesome/free-solid-svg-icons"
+import styles from '../../styles/Editor.module.scss'
 const MonacoEditorWithNoSSR = dynamic( 
     async () => import('@monaco-editor/react'),
     { ssr: false }
@@ -16,6 +20,7 @@ export const getServerSideProps = async (context) => {
     };
 };
 export default function Editor() {
+    const [language, setLanguage] = useState('javascript')
     const [playground, setPlayground] = useState('')
     const router = useRouter()
     const { roomId } = router.query
@@ -34,18 +39,24 @@ export default function Editor() {
         }
     }, [db])
     return <>
-    <MonacoEditorWithNoSSR
-        theme="vs-dark"
-        height="100vh"
-        value={playground}
-        defaultLanguage="javascript"
-        onChange={(newValue, { changes }) => {
-            const [{ rangeLength, rangeOffset, text }] = changes;
-            if(rangeOffset === 0 && rangeLength === text.length) {
-                return;
-            }
-            updateByPath(`rooms/${roomId}/playground`, newValue)
-        }}
-    />
+        <header className={styles.language_group}>
+            <FontAwesomeIcon className={language === 'javascript' ? 'selected' : ''} icon={faJs} onClick={()=>{setLanguage('javascript')}}/>
+            <FontAwesomeIcon className={language === 'java' ? 'selected' : ''} icon={faJava} onClick={()=>{setLanguage('java')}}/>
+            <FontAwesomeIcon className={language === 'python' ? 'selected' : ''} icon={faPython} onClick={()=>{setLanguage('python')}}/>
+            <FontAwesomeIcon className={language === 'sql' ? 'selected' : ''} icon={faDatabase} onClick={()=>{setLanguage('sql')}}/>
+        </header>
+        <MonacoEditorWithNoSSR
+            theme="vs-dark"
+            height="100vh"
+            value={playground}
+            language={language}
+            onChange={(newValue, { changes }) => {
+                const [{ rangeLength, rangeOffset, text }] = changes;
+                if(rangeOffset === 0 && rangeLength === text.length) {
+                    return;
+                }
+                updateByPath(`rooms/${roomId}/playground`, newValue)
+            }}
+        />
     </>
 }
