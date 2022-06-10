b4ad72b1168dfe60a69750165f9a9dbd9b133ad9
rheehoonkyung <9398216@ict-companion.com>
2022-06-10 13:57:37 +0900

    [수정] 방을 선택했을때만 방 표시

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import styles from '../styles/Home.module.scss'
 import RoomList from '../components/RoomList'
 import Room from '../components/Room'
+import { useRecoilValue } from 'recoil'
+import { roomIdState } from '../recoil/atoms'
 
 export default function Home() {
+  const roomId = useRecoilValue(roomIdState)
   return <>
     <div className={styles.titlebar}>KiyomiTalk</div>
     <div className={styles.container}>
@@ -11,7 +14,7 @@ export default function Home() {
         <RoomList/>
       </header>
       <main className={styles.main}>
-        <Room/>
+        {roomId && <Room/>}
       </main>
     </div>
   
