274983d02cf33af6c6384eca06ffcca3da59b23f
HoonKyung, Rhee <yebrwe@naver.com>
2022-09-21 15:42:58 +0900

    fix _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,19 @@
 import '../styles/globals.css'
 import { RecoilRoot } from 'recoil';
 import LoginChecker from '../components/common/LoginChecker';
+import Script from 'next/script';
 function MyApp({ Component, pageProps }) {
   return (
     <RecoilRoot>
       <LoginChecker/>
       <Component {...pageProps} />
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4111350052400529" crossorigin="anonymous"></script>
+      <Script id="Adsense-id"
+        data-ad-client="ca-pub-4111350052400529"
+        async="true"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      
     </RecoilRoot>
   )
 }
