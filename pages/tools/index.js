eb0f8f5f80ae5586e14b3a542cc21aaa6ce3cbb0
rheehoonkyung <9398216@ict-companion.com>
2022-12-28 16:28:46 +0900

    네이버 공백미포함 글자수세기추가

--- pages/tools/index.js
+++ pages/tools/index.js
@@ -2,6 +2,37 @@ import {useEffect, useState} from "react";
 export default function Tools() {
     const [letterCount, setLetterCount] = useState(0)
     var oEditors = [];
+
+    function search() {
+        const {KMR, KKMA} = require('koalanlp/API');
+        const {initialize} = require('koalanlp/Util');
+        const {Tagger, Parser} = require('koalanlp/proc');
+
+        async function executor(){
+            await initialize({packages: {KMR: '2.0.4', KKMA: '2.0.4'}, verbose: true});
+
+            let tagger = new Tagger(KMR);
+            let tagged = await tagger("안녕하세요. 눈이 오는 설날 아침입니다.");
+            for(const sent of tagged) {
+                console.log(sent.toString());
+            }
+
+            let parser = new Parser(KKMA);
+            let parsed = await parser("안녕하세요. 눈이 오는 설날 아침입니다.");
+            for(const sent of parsed){
+                console.log(sent.toString());
+                for(const dep of sent.dependencies){
+                    console.log(dep.toString());
+                }
+            }
+        }
+
+        executor().then(
+            () => console.log('finished!'),
+            (error) => console.error('Error Occurred!', error)
+        );
+    }
+    search();
     useEffect(()=>{
         window.nhn.husky.EZCreator.createInIFrame({
             oAppRef: oEditors,
@@ -23,7 +54,7 @@ export default function Tools() {
                 justifyContent: "flex-start"
             }}
         >
-            <div style={{width:"25%"}}>
+            <div style={{width:"35%"}}>
                 <textarea name="weditor" id="weditor" style={{ width: "500px" , height: '700px', maxHeight: '700px', border: "1px solid #DDDDDD", borderRadius: "4px", padding: "10px", overflow: "auto"}}></textarea>
 
                 <div style={{display: "flex", justifyContent:"flex-end"}}>
